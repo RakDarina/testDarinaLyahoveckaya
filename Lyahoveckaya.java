110bb6ad4cab0a0c53e431c19f5dc1cc9643a73f
RakDarina <129872476+RakDarina@users.noreply.github.com>
2026-03-25 23:06:50 +0400

    Update Lyahoveckaya.java

--- Lyahoveckaya.java
+++ Lyahoveckaya.java
@@ -4,30 +4,30 @@ public class Lyahoveckaya {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите число a: ");
+        System.out.print("Enter number a: ");
         int a = scanner.nextInt();
 
-        System.out.print("Введите число b: ");
+        System.out.print("Enter number b: ");
         int b = scanner.nextInt();
 
         // Сравнение
         if (a > b) {
-            System.out.println("a > b");
+            System.out.println(a + " > " + b);
         } else if (a < b) {
-            System.out.println("a < b");
+            System.out.println(a + " < " + b);
         } else {
-            System.out.println("a = b");
+            System.out.println(a + " = " + b);
         }
 
         // Арифметика
-        System.out.println("Сложение: " + (a + b));
-        System.out.println("Вычитание: " + (a - b));
-        System.out.println("Умножение: " + (a * b));
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
 
-        if (b != 0) {
-            System.out.println("Деление: " + (double) a / b);
+       if (b != 0) {
+            System.out.println(a + " / " + b + " = " + (double) a / b);
         } else {
-            System.out.println("Деление на ноль невозможно");
+            System.out.println("Division: Cannot divide by zero");
         }
 
         scanner.close();
