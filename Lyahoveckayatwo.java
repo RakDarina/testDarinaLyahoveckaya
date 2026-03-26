bc2259734d53f8dfd03182408e6c57ce136f683e
RakDarina <129872476+RakDarina@users.noreply.github.com>
2026-03-26 13:38:20 +0400

    Update Lyahoveckayatwo.java

--- Lyahoveckayatwo.java
+++ Lyahoveckayatwo.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;
 
-public class Lyahoveckayatwo.java {
+public class Lyahoveckayatwo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Ввод первой строки
         System.out.print("Введите первую строку (a): ");
         String a = scanner.nextLine();
 
-        // Ввод второй строки
         System.out.print("Введите вторую строку (b): ");
         String b = scanner.nextLine();
 
-        // Сравнение строк
         if (a.equals(b)) {
             System.out.println("Строки идентичны");
         } else {
