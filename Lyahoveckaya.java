c00e7edaa53e10ea69674e20baced1e6af6bddd6
RakDarina <129872476+RakDarina@users.noreply.github.com>
2026-03-26 13:27:58 +0400

    Update Lyahoveckaya.java

--- Lyahoveckaya.java
+++ Lyahoveckaya.java
@@ -4,13 +4,14 @@ public class Lyahoveckaya {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter number a: ");
+
+        System.out.print("Введите целое число a: ");
         int a = scanner.nextInt();
 
-        System.out.print("Enter number b: ");
+        System.out.print("Введите второе число b: ");
         int b = scanner.nextInt();
 
-        // Сравнение
+        // В задании непонятно. Нужно чтобы выводило a=b или 5=5. Сделала 2 вариант, но могу и поменять на ("a = b")
         if (a > b) {
             System.out.println(a + " > " + b);
         } else if (a < b) {
@@ -19,7 +20,7 @@ public class Lyahoveckaya {
             System.out.println(a + " = " + b);
         }
 
-        // Арифметика
+
         System.out.println(a + " + " + b + " = " + (a + b));
         System.out.println(a + " - " + b + " = " + (a - b));
         System.out.println(a + " * " + b + " = " + (a * b));
@@ -27,7 +28,7 @@ public class Lyahoveckaya {
        if (b != 0) {
             System.out.println(a + " / " + b + " = " + (double) a / b);
         } else {
-            System.out.println("Division: Cannot divide by zero");
+            System.out.println("На ноль не делят");
         }
 
         scanner.close();
